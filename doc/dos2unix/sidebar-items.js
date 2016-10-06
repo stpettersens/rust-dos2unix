9e623ff534cf70c1922c3958465dced62bd4be06
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-10-06 20:31:09 +0100

    publish documentation.

---
+++ doc/dos2unix/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Dos2Unix",""]]});
---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["dos2unix"] = {"doc":"Rust library to convert DOS to Unix line endings.","items":[[3,"Dos2Unix","dos2unix","",null,null],[11,"convert","","",0,{"inputs":[{"name":"str"},{"name":"bool"}],"output":{"name":"bool"}}]],"paths":[[3,"Dos2Unix"]]};
+initSearch(searchIndex);
